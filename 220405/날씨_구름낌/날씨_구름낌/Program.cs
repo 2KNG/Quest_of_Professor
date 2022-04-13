5c31c86f3abd87ce7755a019ee0fb9c9d40c3fa3
KangHyun Kim <khkim1207@naver.com>
2022-04-13 18:34:30 +0900

    벚꽃과 함께하는 알고리즘

---
+++ 220405/날씨_구름낌/날씨_구름낌/Program.cs
@@ -0,0 +1,260 @@
+﻿// 파이썬으로 하면 진짜 한줄임
+// print(input(eval()))
+
+// C# 코드입니다.
+
+using System.Text.RegularExpressions;
+//string input = Console.ReadLine();
+
+string input = "10-20+10";
+//string input = "1+1*(1*(1+1)/1)*((10+10*1)/1)"; // 41
+//string input = "(1+1*1)+((1+1)/1)*((15-10*1)/1)"; // 12
+int start_point = 0;
+int end_point = 0;
+string calc_point;
+int calc_index = 0;
+int temp_int_one = 0;
+int temp_int_two = 0;
+string temp_one;
+string temp_two;
+
+char[] operChars = { '+', '-', '*', '/', '(', ')' };
+string[] calcValue = input.Split(operChars);
+//char[] calcOper = calcValue.Split();
+//string calcOper = Regex.Replace(input, @"[0-9]", "", RegexOptions.Singleline);
+//Console.WriteLine(calcOper);
+//calcOper.ToCharArray();
+double calc = 0;
+
+
+
+while (input.Contains('('))                                         // 괄호가 있다면 
+{
+    start_point = input.LastIndexOf('(') + 1;                         // 마지막 여는 괄호에서 
+    end_point = input[start_point..].IndexOf(')') + start_point;      // 가장 가까운 닫는 괄호까지 
+    calc_point = input[start_point..end_point];                     // 묶은 것이 가장 먼저 계산할 목표 
+    int input_len = input.Length;
+    Console.WriteLine(calc_point);
+    while (true)
+    {
+        if (calc_point.Contains('*'))                               // 연산 우선 순위에 따라 곱셈부터 
+        {
+            calc_index = calc_point.IndexOf('*');
+
+            temp_one = calc_point[..calc_index].Split(operChars)[^1];
+            temp_two = calc_point[calc_index..].Split(operChars)[1];
+
+            temp_int_one = calc_point[..calc_index].Split(operChars)[^1].Length;         // 좌항 자릿수 
+            temp_int_two = calc_point[calc_index..].Split(operChars)[1].Length;          // 우항 자릿수 
+            // 식을 간소화하기 위해선 인덱스가 필요하기 때문에 * 기준으로 양옆 수의 길이를 저장 
+
+
+            temp_int_one = calc_index - temp_int_one;               // calc_point 를 초기화 시키기 위해 인덱스 저장 
+            temp_int_two = calc_index + temp_int_two + 1;
+
+            calc = int.Parse(temp_one) * int.Parse(temp_two);
+
+            calc_point = calc_point[..temp_int_one] + calc + calc_point[temp_int_two..];    // 계산된 식은 값으로 바꿔서 반환
+            Console.WriteLine(calc_point);
+        }
+        else if (calc_point.Contains('/'))
+        {
+            calc_index = calc_point.IndexOf('/');
+
+            temp_one = calc_point[..calc_index].Split(operChars)[^1];
+            temp_two = calc_point[calc_index..].Split(operChars)[1];
+
+            temp_int_one = calc_point[..calc_index].Split(operChars)[^1].Length;         // 좌항 자릿수 
+            temp_int_two = calc_point[calc_index..].Split(operChars)[1].Length;          // 우항 자릿수 
+
+
+            temp_int_one = calc_index - temp_int_one;               // calc_point 를 초기화 시키기 위해 인덱스 저장 
+            temp_int_two = calc_index + temp_int_two + 1;
+
+            calc = int.Parse(temp_one) / int.Parse(temp_two);
+
+            calc_point = calc_point[..temp_int_one] + calc + calc_point[temp_int_two..];    // 계산된 식은 값으로 바꿔서 반환
+            Console.WriteLine(calc_point);
+        }
+        else if (calc_point.Contains('+'))
+        {
+            calc_index = calc_point.IndexOf('+');
+
+            temp_one = calc_point[..calc_index].Split(operChars)[^1];
+            temp_two = calc_point[calc_index..].Split(operChars)[1];
+
+            temp_int_one = calc_point[..calc_index].Split(operChars)[^1].Length;         // 좌항 자릿수 
+            temp_int_two = calc_point[calc_index..].Split(operChars)[1].Length;          // 우항 자릿수 
+
+
+            temp_int_one = calc_index - temp_int_one;               // calc_point 를 초기화 시키기 위해 인덱스 저장 
+            temp_int_two = calc_index + temp_int_two + 1;
+
+            calc = int.Parse(temp_one) + int.Parse(temp_two);
+
+            calc_point = calc_point[..temp_int_one] + calc + calc_point[temp_int_two..];    // 계산된 식은 값으로 바꿔서 반환
+            Console.WriteLine(calc_point);
+        }
+        else if (calc_point.Contains('-'))
+        {
+            calc_index = calc_point.IndexOf('-');
+
+            temp_one = calc_point[..calc_index].Split(operChars)[^1];
+            temp_two = calc_point[calc_index..].Split(operChars)[1];
+
+            temp_int_one = calc_point[..calc_index].Split(operChars)[^1].Length;         // 좌항 자릿수 
+            temp_int_two = calc_point[calc_index..].Split(operChars)[1].Length;          // 우항 자릿수 
+
+
+            temp_int_one = calc_index - temp_int_one;               // calc_point 를 초기화 시키기 위해 인덱스 저장 
+            temp_int_two = calc_index + temp_int_two + 1;
+
+            calc = int.Parse(temp_one) - int.Parse(temp_two);
+
+            calc_point = calc_point[..temp_int_one] + calc + calc_point[temp_int_two..];    // 계산된 식은 값으로 바꿔서 반환
+            Console.WriteLine(calc_point);
+        }
+        else // 연산자가 더 이상 없으면 
+        {
+            start_point = start_point - 1; // 괄호를 제외해주기 위해 -1
+            if (end_point == input_len - 1) // index error 발생 > 값이 마지막일 때 예외처리 필요!!  모름 …
+            {
+                input = input[..start_point] + calc_point;
+            }
+            else
+            {
+                end_point = end_point + 1; // 괄호를 제외해주기 위해 +1
+                input = input[..start_point] + calc_point + input[end_point..];
+            }
+            break;
+        }
+    }
+    //Console.WriteLine(calc_point);
+}
+
+// 괄호가 없는 남은 값 계산 
+
+while (true)
+{
+    if (input.Contains('*'))                                                    // input 값을 직접 변
+    {
+        calc_index = input.IndexOf('*');
+
+        temp_one = input[..calc_index].Split(operChars)[^1];
+        temp_two = input[calc_index..].Split(operChars)[1];
+
+        temp_int_one = input[..calc_index].Split(operChars)[^1].Length;         // 좌항 자릿수 
+        temp_int_two = input[calc_index..].Split(operChars)[1].Length;          // 우항 자릿수 
+
+
+        temp_int_one = calc_index - temp_int_one;
+        temp_int_two = calc_index + temp_int_two + 1;
+
+        calc = int.Parse(temp_one) * int.Parse(temp_two);
+
+        input = input[..temp_int_one] + calc + input[temp_int_two..];    // 계산된 식은 값으로 바꿔서 반환
+        Console.WriteLine(input);
+    }
+    else if (input.Contains('/'))                                                    // input 값을 직접 변
+    {
+        calc_index = input.IndexOf('/');
+
+        temp_one = input[..calc_index].Split(operChars)[^1];
+        temp_two = input[calc_index..].Split(operChars)[1];
+
+        temp_int_one = input[..calc_index].Split(operChars)[^1].Length;         // 좌항 자릿수 
+        temp_int_two = input[calc_index..].Split(operChars)[1].Length;          // 우항 자릿수 
+
+
+        temp_int_one = calc_index - temp_int_one;
+        temp_int_two = calc_index + temp_int_two + 1;
+
+        calc = int.Parse(temp_one) / int.Parse(temp_two);
+
+        input = input[..temp_int_one] + calc + input[temp_int_two..];    // 계산된 식은 값으로 바꿔서 반환
+        Console.WriteLine(input);
+    }
+    else if (input.Contains('-'))                                                    // input 값을 직접 변
+    {
+        calc_index = input.IndexOf('-');
+
+        if (input.IndexOf('-') != 0)
+        {
+
+            temp_one = input[..calc_index].Split(operChars)[^1];
+            temp_two = input[calc_index..].Split(operChars)[1];
+
+            temp_int_one = input[..calc_index].Split(operChars)[^1].Length;         // 좌항 자릿수 
+            temp_int_two = input[calc_index..].Split(operChars)[1].Length;          // 우항 자릿수 
+
+
+            temp_int_one = calc_index - temp_int_one;
+            temp_int_two = calc_index + temp_int_two + 1;
+
+            calc = int.Parse(temp_one) - int.Parse(temp_two);
+
+            input = input[..temp_int_one] + calc + input[temp_int_two..];    // 계산된 식은 값으로 바꿔서 반환
+            Console.WriteLine(input);
+        }
+
+        else
+        {
+            if (input.Split(operChars).Length == 2)
+            {
+                Console.WriteLine(input);
+                break;
+            }
+            else
+            {
+                string calcOper = Regex.Replace(input, @"[0-9]", "", RegexOptions.Singleline);
+
+                temp_one = input[calc_index..].Split(operChars)[1];
+                temp_two = input[calc_index..].Split(operChars)[2];
+
+                temp_int_one = input[calc_index..].Split(operChars)[1].Length;         // 좌항 자릿수 
+                temp_int_two = input[calc_index..].Split(operChars)[2].Length;          // 우항 자릿수
+
+                int temp_int_sum = temp_int_one + temp_int_two + 2;
+
+                if (calcOper[1] == '+')
+                {
+                    calc = -int.Parse(temp_one) + int.Parse(temp_two);
+                }
+                else if (calcOper[1] == '-')
+                {
+                    calc = -int.Parse(temp_one) - int.Parse(temp_two);
+                }
+
+                input = calc + input[temp_int_sum..];    // 계산된 식은 값으로 바꿔서 반환
+                Console.WriteLine(calc);
+
+                Console.WriteLine(input);
+            }
+        }
+    }
+    else if (input.Contains('+'))                                                    // input 값을 직접 변
+    {
+        calc_index = input.IndexOf('+');
+
+        temp_one = input[..calc_index].Split(operChars)[^1];
+        temp_two = input[calc_index..].Split(operChars)[1];
+
+        temp_int_one = input[..calc_index].Split(operChars)[^1].Length;         // 좌항 자릿수 
+        temp_int_two = input[calc_index..].Split(operChars)[1].Length;          // 우항 자릿수 
+
+
+        temp_int_one = calc_index - temp_int_one;
+        temp_int_two = calc_index + temp_int_two + 1;
+
+        calc = int.Parse(temp_one) + int.Parse(temp_two);
+
+        input = input[..temp_int_one] + calc + input[temp_int_two..];    // 계산된 식은 값으로 바꿔서 반환
+        Console.WriteLine(input);
+    }
+    else // 연산자가 더 이상 없으면 
+    {
+        Console.WriteLine(input);
+        break;
+    }
+}
+
---
+++ 220412/날씨_맑음/날씨_맑음/Program.cs
@@ -0,0 +1,106 @@
+﻿internal class Program
+{
+    class Quest
+    {
+        public int[] Max_return()
+        {
+            string[] input = Console.ReadLine().Split(" ");
+
+            int max_value = -1;
+            int max_index = -1;
+            int value = -1;
+            int index_count = 0;
+
+            foreach (var item in input)
+            {
+                index_count++; // 1부터 시작하기
+                value = int.Parse(item);
+                if (value > max_value)
+                {
+                    max_value = value;
+                    max_index = index_count;
+                }
+            }
+
+            Console.WriteLine("최대값 = " + max_value + "\n" + max_index + "번째에 위치");
+
+            int[] max_Values = { max_value, max_index };
+
+            return max_Values;
+        }
+
+        public int Cost_of_Add(string defalut_input = "뀨")
+        {
+
+            List<int> add_list = new List<int>();
+            List<int> min_sum_list = new List<int>();
+            int count = 0;
+            int temp_sum = 0;
+            int min_index = 0;
+            int min_sum = 0;
+            int temp = 0;
+            string[] input;
+            
+            if (defalut_input == "뀨")
+            {
+                input = Console.ReadLine().Split(" ");
+            }
+            else
+            {
+                input = defalut_input.Split(" ");
+            }
+
+            count = input.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                temp = int.Parse(input[i]);
+                add_list.Add(temp);
+                if (i == 0)
+                {
+                    min_sum_list.Add(temp);
+                }
+                else
+                {
+                    for (min_index = 0; min_index < i; min_index++)
+                    {
+                        if (temp > min_sum_list[min_index])
+                        {
+                            continue;
+                        }
+                        break;
+                    }
+                    min_sum_list.Insert(min_index, temp); // Sort를 사용하는 방법도 있음
+                }
+
+
+                temp_sum = temp_sum + add_list[i] * (count - i); // [i]*(n-1) + [i+1]*(n-1) + [i+2]*(n-2) + [i+3]*(n-3) ... + [count-1]
+            }
+            Console.Write("최소비용 계산 순서 : ");
+            for (int i = 0; i < count; i++)
+            {
+                min_sum = min_sum + min_sum_list[i] * (count - i);
+                Console.Write(min_sum_list[i]+" ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("순서대로 연산 시 : " + (temp_sum - add_list[0]));
+            Console.WriteLine("최소비용 연산 시 : " + (min_sum - min_sum_list[0]));
+            return min_sum;
+        }
+    }
+
+    static void Main(string[] arg)
+    {
+        Quest q = new Quest();
+        //q.Max_return();
+        //q.Cost_of_Add();
+        q.Cost_of_Add("1 2 3");
+        q.Cost_of_Add("3 2 1");
+        q.Cost_of_Add("1 3 2");
+        q.Cost_of_Add("1 2 3 4 6");
+        q.Cost_of_Add("3 6 4 8 9 7");
+        q.Cost_of_Add("1 1 4 2 8 8 8");
+    }
+    
+}
